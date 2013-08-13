76749bfc22fec8918a5a3645a6ab8d2cf7d0e0ab
danyyil-pelekhatyi <pelex.daniell@gmail.com>
2013-08-13 18:25:41 +0300

    PreTests2

--- Resume.Tests/Tests/RepositoryTest.cs
+++ Resume.Tests/Tests/RepositoryTest.cs
@@ -1,4 +1,7 @@
-﻿using Resume.Infrastructure.Repositories;
+﻿using System;
+using System.Linq;
+using Resume.Core.Interfaces;
+using Resume.Infrastructure.Repositories;
 using NUnit.Framework;
 
 namespace Resume.Tests.Tests
@@ -6,9 +9,24 @@ namespace Resume.Tests.Tests
     [TestFixture]
     public class RepositoryTest
     {
-        [Test]
-        public void Abc()
+        public class MyClass
         {
+            public int Val { get; set; }
+        }
+
+        private MyClass createMyClass()
+        {
+            return new MyClass { Val = 33};
+        }
+
+        private IRepository<MyClass> _repository = new Repository<MyClass>();
+
+        [Test]
+        public void AddFindUpdateDeleteTest()
+        {
+            MyClass item = createMyClass();
+            _repository.Add(item);
+            item.Val *= 2;
 
         }
     }
